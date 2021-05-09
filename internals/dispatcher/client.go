25f30645d1c391bbb36ba180764923df0afa3bf1
Hamza Khan <h.akhan@outlook.com>
2021-05-09 00:40:36 -0700

    refactored dispatcher logic to use channels

--- internals/dispatcher/client.go
+++ internals/dispatcher/client.go
@@ -7,34 +7,39 @@ import (
 
 type Client struct {
 	terminate chan struct{}
+	work      chan net.Conn
 	log       *logging.Client
 }
 
 func NewClient(log *logging.Client) *Client {
-	return &Client{terminate: make(chan struct{}, 1), log: log}
+	return &Client{terminate: make(chan struct{}, 1), log: log, work: make(chan net.Conn, 10)}
 }
 
 func (c *Client) Start() {
 
 	c.log.Infof("Dispatcher starting")
 
-	ln, err := net.Listen("tcp", ":8080")
-	if err != nil {
-		// handle error
-	}
+	go func() {
+
+		ln, err := net.Listen("tcp", ":8080")
+		if err != nil {
+			// handle error
+		}
+
+		for {
+			conn, _ := ln.Accept()
+			c.work <- conn
+		}
+	}()
 
 	for {
 		select {
 		case <-c.terminate:
 			// exit loop
+			c.log.Infof("Terminate Recv")
 			return
-		default:
-			conn, err := ln.Accept()
-			if err != nil {
-				// handle error
-			}
-			go c.handleConnection(conn)
-
+		case work := <-c.work:
+			go c.handleConnection(work)
 		}
 	}
 
